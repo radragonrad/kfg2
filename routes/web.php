7eee38fa6b1eadc84e4092b8610bde85dea2e9e1
Rafa Aragon <radragon.rad@gmail.com>
2021-09-29 07:45:13 +0200

    rutas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/',function(){
+    return 'Home';
+});
+
+Route::get('/usuarios',function(){
+    return 'Usuarios';
+});
+
+Route::get('/usuarios/{id}',function($id){
+   return 'Mostrando detalle de usuario : '.$id ;
+})->where('id','[0-9]+');
+
+Route::get('/usuarios/new',function(){
+    return 'Creando usuario nuevo';
+});
+
+Route::get('/usuarios/{name}/{nickname?}',function($name,$nickname=null){
+    return "Doble ruta {$name} con apodo {$nickname}";
 });
